6414905438800f2587e218b28d059db92b68629c
peter0618 <peterchoi0618@gmail.com>
2020-07-15 01:20:46 +0900

    ngFor 문 정리

--- README.md
+++ README.md
@@ -26,3 +26,13 @@ Angular 내장 지시자에 대한 내용을 정리합니다.
 
 ### ngStyle
 ngStyle을 사용하면, DOM 요소의 CSS 프로퍼티를 동적으로 설정할 수 있습니다.
+
+### ngFor
+일반 프로그래밍에서의 for 문 처럼 사용할 수 있습니다.
+해당 태그가 for 문을 돌며, item 갯수 만큼 만들어집니다. (다중 ngFor도 사용 가능합니다.)
+```bash
+<div class="ui list" *ngFor="let c of cities; let num = index">
+  <div class="item">{{ num+1 }} - {{ c }}</div>
+</div>
+```
+위와 같이 for 문의 index에 접근할 수도 있습니다. (index는 0부터 시작합니다.)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,31 @@ export class AppComponent {
   color = 'green';
   fontSize = 0;
 
+  cities = ['Miami', 'Sao Paulo', 'New York'];
+
+  people = [
+    { name: 'Peter', age: '35', city: 'Seoul'},
+    { name: 'Grace', age: '33', city: 'New York'},
+    { name: 'Joseph', age: '32', city: 'Tokyo'},
+  ];
+
+  peopleByCity = [
+    {
+      city: 'Miami',
+      people: [
+        { name: 'John', age: 12 },
+        { name: 'Angel', age: 22 },
+      ]
+    },
+    {
+      city: 'Sao Paulo',
+      people: [
+        { name: 'Anderson', age: 35 },
+        { name: 'Felipe', age: 36 },
+      ]
+    },
+  ];
+
   nextChoice() {
     const next = this.choice + 1;
     this.choice = next > 5 ? 1 : next;
